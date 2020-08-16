b7e878c9bbf1de594ee4c461ea1a2b863a804219
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-16 20:22:09 +0000

    Deploy SmallDreamDev/small-dream-cliente to github.com/SmallDreamDev/small-dream-cliente.git:gh-pages

---
+++ precache-manifest.65b5bed9de7360cb0656057e9b5de8c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ffc6ba8cdd1b91219c15f4a2d27cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93687dd9690bad8246ae",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8856a3722aba21d7a3d0",
+    "url": "/static/js/2.26183bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26183bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93687dd9690bad8246ae",
+    "url": "/static/js/main.f569f638.chunk.js"
+  },
+  {
+    "revision": "e687c330cd1e0bdfa0ab",
+    "url": "/static/js/runtime-main.c75ced87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
