3ff8d160c1ce216da9af97fd6700c22a2498a894
Adrián <adrianoelmagno@gmail.com>
2020-09-07 18:31:46 +0200

    Acabada vista de gestión

--- src/utils/entityManager.js
+++ src/utils/entityManager.js
@@ -1,28 +1,45 @@
 import React from "react";
+import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { CustomCheckbox } from "./../components/CustomCheckbox";
+import { v4 } from "uuid";
 
 class AbstractManager {
     constructor() {
         this.headers = [];
     }
 
-    getHeaders() {
-        return [...this.headers].unshift("#");
+    getHeaders(headerCheckbox, handler) {
+        let clone = [...this.headers];
+        clone.unshift("#");
+        clone.unshift(<Form.Check key={v4()} ref={headerCheckbox} type="checkbox" onChange={handler}/>);
+        return clone;
+    }
+
+    createCheckbox(checkboxes, index){
+        let checkbox = React.createRef();
+        checkboxes.push(checkbox);
+        let cb = <CustomCheckbox id={index} ref={checkbox}/>;
+        return cb;
     }
 }
 
 class ActivityManager extends AbstractManager {
     constructor() {
         super();
-        this.headers = ["Nombre", "Zona"];
+        this.headers = ["Nombre", "Zona", "Categorías asociadas", "Materiales usados"];
     }
 
-    process(entry) {
+    process(entry, index, checkboxes) {
         const { _id, nombre, zona } = entry;
         return (
-            <tr>
-                <td>{_id}</td>
+            <tr key={v4()}>
+                <td>{super.createCheckbox(checkboxes, index)}</td>
+                <td><Link id={index} key={v4()} to={`/detalles?collection=actividades&id=${_id}`}>{index + 1}</Link></td>
                 <td>{nombre}</td>
                 <td>{zona}</td>
+                <td><Link to={`/detalles?collection=actividades&id=${_id}`}>Ver categorías asociadas</Link></td>
+                <td><Link to={`/detalles?collection=actividades&id=${_id}`}>Ver materiales usados</Link></td>
             </tr>
         );
     }
@@ -31,15 +48,17 @@ class ActivityManager extends AbstractManager {
 class CategoryManager extends AbstractManager {
     constructor() {
         super();
-        this.headers = ["Nombre"];
+        this.headers = ["Nombre", "Actividades en esta categoría"];
     }
 
-    process(entry) {
+    process(entry, index, checkboxes) {
         const { _id, nombre } = entry;
         return (
-            <tr>
-                <td>{_id}</td>
+            <tr key={v4()}>
+                <td>{super.createCheckbox(checkboxes, index)}</td>
+                <td><Link id={index} key={v4()} to={`/detalles?collection=categorias&id=${_id}`}>{index + 1}</Link></td>
                 <td>{nombre}</td>
+                <td><Link to={`/detalles?collection=categorias&id=${_id}`}>Ver actividades</Link></td>
             </tr>
         );
     }
@@ -48,18 +67,20 @@ class CategoryManager extends AbstractManager {
 class ClientManager extends AbstractManager {
     constructor() {
         super();
-        this.headers = ["Nombre y apellidos", "DNI", "Contacto", "Fecha de nacimiento"];
+        this.headers = ["Nombre y apellidos", "DNI", "Contacto", "Fecha de nacimiento", "Actividades en las que está apuntado"];
     }
 
-    process(entry) {
+    process(entry, index, checkboxes) {
         const { _id, nombre_completo, dni, contacto, fecha_nacimiento } = entry;
         return (
-            <tr>
-                <td>{_id}</td>
+            <tr key={v4()}>
+                <td>{super.createCheckbox(checkboxes, index)}</td>
+                <td><Link id={index} key={v4()} to={`/detalles?collection=clientes&id=${_id}`}>{index + 1}</Link></td>
                 <td>{nombre_completo}</td>
                 <td>{dni}</td>
                 <td>{contacto}</td>
                 <td>{fecha_nacimiento}</td>
+                <td><Link to={`/detalles?collection=clientes&id=${_id}`}>Ver actividades</Link></td>
             </tr>
         );
     }
@@ -71,11 +92,12 @@ class InstructorManager extends AbstractManager {
         this.headers = ["Nombre y apellidos", "DNI", "Contacto"];
     }
 
-    process(entry) {
+    process(entry, index, checkboxes) {
         const { _id, nombre_completo, dni, contacto } = entry;
         return (
-            <tr>
-                <td>{_id}</td>
+            <tr key={v4()}>
+                <td>{super.createCheckbox(checkboxes, index)}</td>
+                <td><Link id={index} key={v4()} to={`/detalles?collection=monitores&id=${_id}`}>{index + 1}</Link></td>
                 <td>{nombre_completo}</td>
                 <td>{dni}</td>
                 <td>{contacto}</td>
@@ -87,41 +109,45 @@ class InstructorManager extends AbstractManager {
 class MaterialManager extends AbstractManager {
     constructor() {
         super();
-        this.headers = ["Descripción", "Precio"];
+        this.headers = ["Descripción", "Precio", "Actividades que usan este material"];
     }
 
-    process(entry) {
-        const {_id, descripcion, precio} = entry;
+    process(entry, index, checkboxes) {
+        const { _id, descripcion, precio } = entry;
         return (
-            <tr>
-                <td>{_id}</td>
+            <tr key={v4()}>
+                <td>{super.createCheckbox(checkboxes, index)}</td>
+                <td><Link id={index} key={v4()} to={`/detalles?collection=materiales&id=${_id}`}>{index + 1}</Link></td>
                 <td>{descripcion}</td>
                 <td>{precio}</td>
+                <td><Link to={`/detalles?collection=materiales&id=${_id}`}>Ver actividades</Link></td>
             </tr>
         );
     }
 }
 
-class ScheduleManager extends AbstractManager {
+class WorkshopManager extends AbstractManager {
     constructor() {
         super();
-        this.headers = ["# Actividad", "# Monitor", "# Horario"];
+        this.headers = ["# Actividad", "# Monitor", "Fecha", "Hora inicio", "Hora fin", "Plazas totales", "# Modo de pago", "Clientes apuntados"];
     }
 
-    process(entry) {
-        const { _id, id_actividad, id_monitor, id_horario }
-    }
-}
-
-function getManager(collectionName) {
-    switch (collectionName) {
-        case "actividades": return new ActivityManager();
-        case "categorias": return new CategoryManager();
-        case "clientes": return new ClientManager();
-        case "materiales": return new MaterialManager();
-        case "monitores": return new InstructorManager();
-        case "programas": return new ScheduleManager();
-        default: return null;
+    process(entry, index, checkboxes) {
+        const { _id, id_actividad, id_monitor, fecha, hora_inicio, hora_fin, plazas, id_modo_pago } = entry;
+        return (
+            <tr key={v4()}>
+                <td>{super.createCheckbox(checkboxes, index)}</td>
+                <td><Link id={index} key={v4()} to={`/detalles?collection=talleres&id=${_id}`}>{index + 1}</Link></td>
+                <td><Link to={`/detalles?collection=actividades&id=${id_actividad}`}>Ver actividad</Link></td>
+                <td><Link to={`/detalles?collection=monitores${id_monitor}`}>Ver monitor</Link></td>
+                <td>{fecha}</td>
+                <td>{hora_inicio}</td>
+                <td>{hora_fin}</td>
+                <td>{plazas}</td>
+                <td><Link to={`/detalles?collection=modosDePago${id_modo_pago}`}>Ver modo de pago</Link></td>
+                <td><Link to={`/detalles?collection=talleres&id=${_id}`}>Ver clientes apuntados</Link></td>
+            </tr>
+        );
     }
 }
 
@@ -132,8 +158,20 @@ function getCollectionName(guiName) {
         case "Clientes": return "clientes";
         case "Materiales": return "materiales";
         case "Monitores": return "monitores";
-        case "Programaciones": return "programas";
+        case "Programaciones": return "talleres";
         default: return "";
+    }
+}
+
+function getManager(collectionName) {
+    switch (collectionName) {
+        case "actividades": return new ActivityManager();
+        case "categorias": return new CategoryManager();
+        case "clientes": return new ClientManager();
+        case "materiales": return new MaterialManager();
+        case "monitores": return new InstructorManager();
+        case "talleres": return new WorkshopManager();
+        default: return null;
     }
 }
 
--- src/views/EntityManagementView.js
+++ src/views/EntityManagementView.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Container, Form, Button, InputGroup, FormControl, Table } from "react-bootstrap";
+import { Container, Form, Button, InputGroup, FormControl, Table, Alert, Modal } from "react-bootstrap";
 import { AbstractComponent } from "./../components/AbstractComponent";
 import { getManager, getCollectionName } from "./../utils/entityManager";
+import { v4 } from "uuid";
+import history from "../utils/history";
 
 class EntityManagementView extends AbstractComponent {
 
@@ -12,39 +14,135 @@ class EntityManagementView extends AbstractComponent {
             entityGUIName: "",
             tableHeaders: [],
             tableEntries: [],
+            allEntries: [],
+            deletionErrorMessage: "",
+            isModalOpen: false,
         }
+        this.checkboxes = [];
+        this.checkboxesClone = [];
         this.manager = null;
+        this.headerCheckbox = React.createRef();
         this.loadTable = this.loadTable.bind(this);
+        this.changeTable = this.changeTable.bind(this);
         this.buildRow = this.buildRow.bind(this);
+        this.toggleCheckboxes = this.toggleCheckboxes.bind(this);
+        this.filterBySearchBar = this.filterBySearchBar.bind(this);
+        this.handleAddEntity = this.handleAddEntity.bind(this);
+        this.handleDeleteSelected = this.handleDeleteSelected.bind(this);
+        this.renderEntityDeletionError = this.renderEntityDeletionError.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     componentDidMount() {
         this.loadTable();
     }
 
-    buildRow(entry) {
-        return this.manager.process(entry);
+    buildRow(entry, i) {
+        return this.manager.process(entry, i, this.checkboxes);
+    }
+
+    toggleCheckboxes() {
+        if (this.headerCheckbox.current.checked) {
+            this.checkboxes.forEach(function (chbx) {
+                if (chbx.current !== null) {
+                    chbx.current.toggleCheck(true);
+                }
+            });
+        } else {
+            this.checkboxes.forEach(function (chbx) {
+                if (chbx.current !== null) {
+                    chbx.current.toggleCheck(false);
+                }
+            });
+        }
+    }
+
+    filterBySearchBar() {
+        let input = document.getElementById("inlineFormInputGroup").value;
+        let filteredEntries = [];
+        this.state.allEntries.forEach(function (e) {
+            for (let key in e) {
+                if (typeof e[key] === "string" && e[key].toLowerCase().includes(input.toLowerCase())) {
+                    filteredEntries.push(e);
+                    break;
+                }
+            }
+        });
+        this.setState({ tableEntries: filteredEntries });
+    }
+
+    handleAddEntity() {
+        history.push("/crear?collection=" + this.state.currentEntity);
+    }
+
+    handleDeleteSelected() {
+        let deleteEntry = super.getAPIManager().deleteEntity;
+        let _this = this;
+        for (let i = 0; i < this.checkboxesClone.length; i++) {
+            let listElement = this.checkboxes.filter(function (c) { return c.current !== null })[i];
+            listElement.current.toggleCheck(this.checkboxesClone[i]);
+        }
+        this.checkboxes.filter(function (chbx) {
+            return chbx.current !== null && chbx.current.checked();
+        }).forEach(function (chbx) {
+            let idCell = document.getElementById(chbx.current.id);
+            let id = idCell.href.split("id=")[1];
+            deleteEntry(id, _this.state.currentEntity, function (isDeleted) {
+                if (isDeleted) {
+                    let tableEntries = _this.state.tableEntries.filter(function (e) { return e._id !== id });
+                    _this.setState({ tableEntries, isModalOpen: false });
+                } else {
+                    _this.setState({ deletionErrorMessage: "Ha habido un error al borrar la/s fila/s seleccionada/s", isModalOpen: false });
+                    _this.renderEntityDeletionError();
+                }
+            });
+        });
+    }
+
+    renderEntityDeletionError() {
+        if (this.state.deletionErrorMessage) {
+            return (
+                <Alert variant="danger">
+                    {this.state.deletionErrorMessage}
+                </Alert>
+            );
+        }
+    }
+
+    openModal() {
+        let selected = this.checkboxes.filter(function (c) { return c.current !== null && c.current.checked(); }).length > 0;
+        if (selected) {
+            this.checkboxesClone = this.checkboxes.filter(function (c) { return c.current !== null }).map(function (c) { return c.current.checked() });
+            this.setState({ isModalOpen: true });
+        }
+    }
+
+    closeModal() {
+        this.setState({ isModalOpen: false });
+    }
+
+    changeTable() {
+
     }
 
     loadTable() {
+        this.checkboxes = [];
         let entityGUIName = document.getElementById("tableSelector")
-            .options[document.getElementById("tableSelector")].value.toLowerCase();
+            .options[document.getElementById("tableSelector").selectedIndex].value;
         let currentEntity = getCollectionName(entityGUIName);
         this.manager = getManager(currentEntity);
-        let tableHeaders = this.manager.getHeaders();
+        let tableHeaders = this.manager.getHeaders(this.headerCheckbox, this.toggleCheckboxes);
 
         let _this = this;
         super.getAPIManager().getEntityList(currentEntity, function (list) {
-            _this.setState({ currentEntity, entityGUIName, tableHeaders, tableEntries: list });
+            _this.setState({ currentEntity, entityGUIName: entityGUIName.toLowerCase(), tableHeaders, tableEntries: list, allEntries: list, deletionErrorMessage: "" });
         });
-
-
     }
 
     render() {
         const { entityGUIName, tableHeaders, tableEntries } = this.state;
         let buildRow = this.buildRow;
-
         return (
             <Container className="p-0">
                 <Container className="p-0">
@@ -59,7 +157,7 @@ class EntityManagementView extends AbstractComponent {
                                 <option>Clientes</option>
                                 <option>Materiales</option>
                                 <option>Monitores</option>
-                                <option>Talleres</option>
+                                <option>Programaciones</option>
                             </Form.Control>
                         </Container>
                         <Container className="col pr-0">
@@ -67,41 +165,56 @@ class EntityManagementView extends AbstractComponent {
                                 <InputGroup.Prepend>
                                     <InputGroup.Text>⌕</InputGroup.Text>
                                 </InputGroup.Prepend>
-                                <FormControl id="inlineFormInputGroup" placeholder="Nombre, apellidos..." />
+                                <FormControl id="inlineFormInputGroup" placeholder="Nombre, apellidos..." onChange={this.filterBySearchBar} />
                             </InputGroup>
                         </Container>
                     </Container>
                     <Container className="row d-flex flex-row-reverse p-0 mx-0 my-2">
-                        <Button className="ml-1" variant="secondary">Borrar seleccionados</Button>
-                        <Button className="mr-1" variant="secondary">Añadir</Button>
+                        <Button className="ml-1" variant="secondary" onClick={this.openModal}>Borrar seleccionados</Button>
+                        <Button className="mr-1" variant="secondary" onClick={this.handleAddEntity}>Añadir</Button>
                     </Container>
                 </Container>
+                <Container className="p-0">
+                    {this.renderEntityDeletionError()}
+                </Container>
                 {
-                    tableEntries.length === 0 ?
+                    tableEntries.length === 0 && entityGUIName ?
                         (<p>No hay { entityGUIName}</p>)
                         :
-                        (<><Table variant="dark" bordered hover responsive/*size="sm"*/>
-
-
+                        (<Table variant="dark" bordered hover responsive>
                             <thead>
                                 <tr>
                                     {
                                         tableHeaders.map(function (h) {
-                                            return <th>{h}</th>;
+                                            return <th key={v4()}>{h}</th>;
                                         })
                                     }
                                 </tr>
                             </thead>
                             <tbody>
                                 {
-                                    tableEntries.map(function (e) {
-                                        return buildRow(e);
+                                    tableEntries.map(function (e, i) {
+                                        return buildRow(e, i);
                                     })
                                 }
                             </tbody>
 
-                        </Table></>)
+                        </Table>)
                 }
+                <Modal show={this.state.isModalOpen} onHide={this.closeModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Borrar seleccionados</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p>¿Estas seguro de que quieres borrar la/s fila/s seleccionada/s?</p>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.closeModal}>No</Button>
+                        <Button variant="primary" onClick={this.handleDeleteSelected}>Sí</Button>
+                    </Modal.Footer>
+                </Modal>
             </Container>
         );
     }
